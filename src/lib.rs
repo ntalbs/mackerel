a32399f498886ade91ffd87c18eaa8ef32bec2c5
ntalbs <ntalbsen@gmail.com>
2025-10-16 19:46:53 +0100

    Add Markerel struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
 mod markdown;
 mod renderer;
 mod scanner;
+
+use markdown::Markdown;
+use scanner::{Scanner, Token};
+
+pub struct Markerel<'a> {
+    scanner: Scanner<'a>,
+}
+
+impl<'a> Markerel<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            scanner: Scanner::new(input),
+        }
+    }
+
+    pub fn scan(&mut self) -> Vec<Token> {
+        self.scanner.scan()
+    }
+
+    pub fn parse(&mut self) -> Markdown {
+        todo!()
+    }
+}
